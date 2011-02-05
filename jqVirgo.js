48d7e0ab5ba8db63beec0205aaba0be61a449936
spaceboy <jiri.votocek@centrum.cz>
2011-02-05 01:57:11 +0100

    first commit

---
+++ jqVirgo.js
@@ -0,0 +1,35 @@
+$.fn.inputVirgo = function (params) {
+  params = $.extend({
+    className: 'virgo',
+    emptyValue: '',
+    textLabel: 'Search'
+  }, params || {});
+  return $(this).each(function (i, el) {
+    var $el = $(el);
+    if (params.emptyValue === $el.val()) {
+      $el
+        .val(params.text)
+        .addClass(params.className);
+    }
+    $el
+      .bind('focus', function () {
+        if ($el.hasClass(params.className)) {
+          $el
+            .removeClass(params.className)
+            .val(params.emptyValue);
+        }
+      })
+      .bind('blur', function (e) {
+        if (params.emptyValue == $el.val()) {
+          $el
+            .val(params.textLabel)
+            .addClass(params.className)
+        }
+      })
+      .closest('form').bind('submit', function () {
+        if ($el.hasClass(params.className)) {
+          $el.val(params.emptyValue);
+        }
+      });
+  });
+};
